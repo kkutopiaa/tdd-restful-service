418afe39bea0ba69e5792d9949c60b0645a915c5
qinxuekuan <qinxuekuan@qianxin.com>
2022-08-23 12:48:50 +0800

    1th。实现 should_use_status_from_response 测试

--- restful-service/src/main/java/com/kuan/rest/ResourceServlet.java
+++ restful-service/src/main/java/com/kuan/rest/ResourceServlet.java
@@ -1,6 +1,11 @@
 package com.kuan.rest;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
 
 public class ResourceServlet extends HttpServlet {
 
@@ -8,5 +13,12 @@ public class ResourceServlet extends HttpServlet {
 
     public ResourceServlet(Runtime runtime) {
         this.runtime = runtime;
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ResourceRouter router = runtime.getResourceRouter();
+        OutboundResponse response = router.dispatch(req, runtime.createResourceContext(req, resp));
+        resp.setStatus(response.getStatus());
     }
 }
