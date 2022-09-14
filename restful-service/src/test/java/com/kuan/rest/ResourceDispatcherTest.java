b9b7105ffd58dd97ac940de261b93b82ad80abc1
qxkk <qinxuekuan@qianxin.com>
2022-09-14 20:24:36 +0800

    49th。整理spike的代码。

--- restful-service/src/test/java/com/kuan/rest/ResourceDispatcherTest.java
+++ restful-service/src/test/java/com/kuan/rest/ResourceDispatcherTest.java
@@ -1,10 +1,6 @@
 package com.kuan.rest;
 
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
-import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.container.ResourceContext;
 import jakarta.ws.rs.core.*;
 import jakarta.ws.rs.ext.RuntimeDelegate;
@@ -12,23 +8,25 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.lang.annotation.Annotation;
-import java.lang.reflect.Method;
 import java.net.URI;
-import java.util.*;
-import java.util.regex.Pattern;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class ResourceDispatcherTest {
     private Runtime runtime;
+    private RuntimeDelegate delegate;
+    private HttpServletRequest request;
+    private ResourceContext context;
 
     @BeforeEach
     public void before() {
         runtime = mock(Runtime.class);
-        RuntimeDelegate delegate = mock(RuntimeDelegate.class);
+        delegate = mock(RuntimeDelegate.class);
         RuntimeDelegate.setInstance(delegate);
 
         when(delegate.createResponseBuilder()).thenReturn(new Response.ResponseBuilder() {
@@ -185,218 +183,13 @@ public class ResourceDispatcherTest {
             }
         });
 
+        request = mock(HttpServletRequest.class);
+        context = mock(ResourceContext.class);
+        when(request.getServletPath()).thenReturn("/users");
     }
 
     @Test
     public void spike() {
-        HttpServletRequest request = mock(HttpServletRequest.class);
-        ResourceContext context = mock(ResourceContext.class);
-
-        when(request.getServletPath()).thenReturn("/users");
-        when(context.getResource(eq(Users.class))).thenReturn(new Users());
-
-        Router router = new Router(runtime, List.of(new ResourceClass(Users.class)));
-        OutboundResponse response = router.dispatch(request, context);
-        GenericEntity<String> entity = (GenericEntity<String>) response.getEntity();
-
-        assertEquals("all", entity.getEntity());
-
-    }
-
-    static class Router implements ResourceRouter {
-
-        private Runtime runtime;
-        private List<Resource> rootResources;
-
-        public Router(Runtime runtime, List<Resource> rootResources) {
-            this.runtime = runtime;
-            this.rootResources = rootResources;
-        }
-
-        @Override
-        public OutboundResponse dispatch(HttpServletRequest request, ResourceContext resourceContext) {
-
-//            runtime.createUriInfoBuilder(request);
-
-            ResourceMethod resourceMethod = rootResources.stream()
-                    .map(root -> root.matches(request.getServletPath(), "GET", new String[0], null))
-                    .filter(Optional::isPresent)
-                    .findFirst()
-                    .get()
-                    .get();
-
-            try {
-                GenericEntity entity = resourceMethod.call(resourceContext, null);
-                return (OutboundResponse) Response.ok(entity).build();
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-
-        }
-    }
-
-
-    static class ResourceClass implements Resource {
-
-        private Pattern pattern;
-        private String path;
-        private Class<?> resourceClass;
-
-        private Map<URITemplate, ResourceMethod> methods = new HashMap<>();
-
-        record URITemplate(Pattern uri, String[] mediaTypes) {
-
-        }
-
-
-        public ResourceClass(Class<?> resourceClass) {
-            this.resourceClass = resourceClass;
-            path = resourceClass.getAnnotation(Path.class).value();
-            pattern = Pattern.compile(path + "(/.*)?");
-
-            for (Method method : Arrays.stream(resourceClass.getMethods())
-                    .filter(m -> m.isAnnotationPresent(GET.class)).toList()) {
-                methods.put(new URITemplate(pattern, method.getAnnotation(Produces.class).value()),
-                        new NormalResourceMethod(resourceClass, method));
-            }
-
-            // 去找 sub resource
-            for (Method method : Arrays.stream(resourceClass.getMethods())
-                    .filter(m -> m.isAnnotationPresent(Path.class)).toList()) {
-                Path path = method.getAnnotation(Path.class);
-                Pattern pattern = Pattern.compile(this.path + ("(/" + path + ")?"));
-                methods.put(new URITemplate(pattern, method.getAnnotation(Produces.class).value()),
-                        new SubResourceLocator(resourceClass, method, new String[0]));
-            }
-
-        }
-
-        @Override
-        public Optional<ResourceMethod> matches(String path, String method, String[] mediaTypes, UriInfoBuilder builder) {
-            if (!pattern.matcher(path).matches()) {
-                return Optional.empty();
-            }
-
-            return methods.entrySet().stream()
-                    .filter(e -> e.getKey().uri.matcher(path).matches())
-                    .map(e -> e.getValue())
-                    .findFirst();
-        }
-    }
-
-    // 还有一种时 sub resource method， 请求之后，返回了另一个资源（转发、重定向）
-    static class NormalResourceMethod implements ResourceMethod {
-
-        private Class<?> resourceClass;
-        private Method method;
-
-        public NormalResourceMethod(Class<?> resourceClass, Method method) {
-            this.resourceClass = resourceClass;
-            this.method = method;
-        }
-
-        @Override
-        public GenericEntity<?> call(ResourceContext resourceContext, UriInfoBuilder builder) {
-            Object resource = resourceContext.getResource(resourceClass);
-
-            try {
-                return new GenericEntity<>(method.invoke(resource), method.getGenericReturnType());
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-
-        }
-    }
-
-
-    static class SubResourceLocator implements ResourceMethod {
-
-        private Class<?> resourceClass;
-        private Method method;
-        private String[] mediaTypes;
-
-        public SubResourceLocator(Class<?> resourceClass, Method method, String[] mediaTypes) {
-            this.resourceClass = resourceClass;
-            this.method = method;
-            this.mediaTypes = mediaTypes;
-        }
-
-        @Override
-        public GenericEntity<?> call(ResourceContext resourceContext, UriInfoBuilder builder) {
-            Object resource = resourceContext.getResource(resourceClass);
-
-            try {
-                Object subResource = method.invoke(resource);
-
-                return new SubResource(subResource).matches(builder.getUnmatchedPath(), "GET", mediaTypes, builder)
-                        .get().call(resourceContext, builder);
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-
-        }
-    }
-
-
-    static class SubResource implements Resource {
-
-        private Object subResource;
-        private Class<? extends Object> subResourceClass;
-
-        private Map<ResourceClass.URITemplate, ResourceMethod> methods = new HashMap<>();
-
-        public SubResource(Object subResource) {
-            this.subResource = subResource;
-            this.subResourceClass = subResource.getClass();
-        }
-
-        @Override
-        public Optional<ResourceMethod> matches(String path, String method, String[] mediaTypes, UriInfoBuilder builder) {
-            return Optional.empty();
-        }
-    }
-
-    interface Resource {
-        Optional<ResourceMethod> matches(String path, String method, String[] mediaTypes, UriInfoBuilder builder);
-    }
-
-    interface RootResource extends Resource {
-        UriTemplate getUriTemplate();
-    }
-
-    interface ResourceMethod {
-        GenericEntity<?> call(ResourceContext resourceContext, UriInfoBuilder builder);
-    }
-
-    @Path("/users")
-    static class Users {
-
-        @GET
-        @Produces(MediaType.TEXT_PLAIN)
-        public String asText() {
-            return "all";
-        }
-
-        @GET
-        @Path("{id}")
-        @Produces(MediaType.TEXT_HTML)
-        public String asHTML(@PathParam("id") int id) {
-            return "all";
-        }
-
-        @Path("/orders")
-        public Orders getOrders() {
-            return new Orders();
-        }
-
-    }
-
-    static class Orders {
-
-        @GET
-        public String asText() {
-            return "all";
-        }
 
     }
 
