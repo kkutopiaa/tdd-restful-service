637ab1ec6788ffa8385cca538bd6082d0ba26cde
qxkk <qinxuekuan@qianxin.com>
2022-09-19 19:19:06 +0800

    50th。整理 spike 的代码

--- restful-service/src/main/java/com/kuan/rest/ResourceRouter.java
+++ restful-service/src/main/java/com/kuan/rest/ResourceRouter.java
@@ -2,9 +2,24 @@ package com.kuan.rest;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.ws.rs.container.ResourceContext;
+import jakarta.ws.rs.core.GenericEntity;
+
+import java.util.Optional;
 
 public interface ResourceRouter {
 
     OutboundResponse dispatch(HttpServletRequest request, ResourceContext resourceContext);
+
+    interface Resource {
+        Optional<ResourceMethod> matches(String path, String method, String[] mediaTypes, UriInfoBuilder builder);
+    }
+
+    interface RootResource extends Resource {
+        UriTemplate getUriTemplate();
+    }
+
+    interface ResourceMethod {
+        GenericEntity<?> call(ResourceContext resourceContext, UriInfoBuilder builder);
+    }
 
 }
