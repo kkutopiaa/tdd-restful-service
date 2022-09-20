7b067d275882145212d63fef71c4f17275d0da9d
qxkk <qinxuekuan@qianxin.com>
2022-09-20 13:30:08 +0800

    test

--- restful-service/src/main/java/com/kuan/rest/OutboundResponse.java
+++ restful-service/src/main/java/com/kuan/rest/OutboundResponse.java
@@ -6,7 +6,6 @@ import jakarta.ws.rs.core.Response;
 import java.lang.annotation.Annotation;
 
 public abstract class OutboundResponse extends Response {
-
     abstract GenericEntity getGenericEntity();
 
     abstract Annotation[] getAnnotations();
