d459b6dbd8ca4f659578da76df27ceaf1969cd7d
qxkk <qinxuekuan@qianxin.com>
2022-09-28 12:49:28 +0800

    57th。feat:解决更改方法签名之后的测试适配。

--- restful-service/src/test/java/com/kuan/rest/ResourceDispatcherTest.java
+++ restful-service/src/test/java/com/kuan/rest/ResourceDispatcherTest.java
@@ -50,7 +50,7 @@ public class ResourceDispatcherTest {
         GenericEntity entity = new GenericEntity("matched", String.class);
 
         ResourceRouter router = new DefaultResourceRoot(runtime, List.of(
-                rootResource(matched("/users/1", result("/1")), returns(entity)),
+                rootResource(matched2("/users/1", result("/1")), returns(entity)),
                 rootResource(unmatched("/users/1"))));
         OutboundResponse response = router.dispatch(request, context);
         assertSame(entity, response.getGenericEntity());
@@ -63,8 +63,8 @@ public class ResourceDispatcherTest {
         GenericEntity entity2 = new GenericEntity("2", String.class);
 
         ResourceRouter router = new DefaultResourceRoot(runtime, List.of(
-                rootResource(matched("/users/1", result("/1", 2)), returns(entity2)),
-                rootResource(matched("/users/1", result("/1", 1)), returns(entity1))));
+                rootResource(matched2("/users/1", result("/1", 2)), returns(entity2)),
+                rootResource(matched2("/users/1", result("/1", 1)), returns(entity1))));
         OutboundResponse response = router.dispatch(request, context);
         assertSame(entity1, response.getGenericEntity());
         assertEquals(200, response.getStatus());
@@ -90,7 +90,7 @@ public class ResourceDispatcherTest {
     @Test
     public void should_return_204_if_method_return_null() {
         ResourceRouter router = new DefaultResourceRoot(runtime,
-                List.of(rootResource(matched("/users/1", result("/1")), returns(null))));
+                List.of(rootResource(matched2("/users/1", result("/1")), returns(null))));
         OutboundResponse response = router.dispatch(request, context);
         assertNull(response.getGenericEntity());
         assertEquals(204, response.getStatus());
@@ -120,6 +120,14 @@ public class ResourceDispatcherTest {
         return matched;
     }
 
+    private ResourceRouter.RootResource rootResource(StubUriTemplate stub, ResourceRouter.ResourceMethod method) {
+        ResourceRouter.RootResource matched = mock(ResourceRouter.RootResource.class);
+        when(matched.getUriTemplate()).thenReturn(stub.uriTemplate);
+        when(matched.match(same(stub.result), eq("GET"), eq(new String[]{MediaType.WILDCARD}), eq(builder)))
+                .thenReturn(Optional.of(method));
+        return matched;
+    }
+
     private ResourceRouter.ResourceMethod returns(GenericEntity entity) {
         ResourceRouter.ResourceMethod method = mock(ResourceRouter.ResourceMethod.class);
         when(method.call(same(context), same(builder))).thenReturn(entity);
@@ -131,6 +139,16 @@ public class ResourceDispatcherTest {
         when(matchedUriTemplate.match(eq(path))).thenReturn(Optional.of(result));
         return matchedUriTemplate;
     }
+
+    private StubUriTemplate matched2(String path, UriTemplate.MatchResult result) {
+        UriTemplate matchedUriTemplate = mock(UriTemplate.class);
+        when(matchedUriTemplate.match(eq(path))).thenReturn(Optional.of(result));
+        return new StubUriTemplate(matchedUriTemplate, result);
+    }
+
+    record StubUriTemplate(UriTemplate uriTemplate, UriTemplate.MatchResult result) {
+    }
+
 
     private UriTemplate.MatchResult result(String path, int order) {
         return new FakeMatchResult(path, order);
