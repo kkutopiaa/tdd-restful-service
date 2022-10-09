343c6d878d3b5915d7fe744c5de91459a9c6bfd9
qxkk <qinxuekuan@qianxin.com>
2022-10-09 13:12:34 +0800

    60th。重构。将 Result 方法向上提一个层级放置

--- restful-service/src/main/java/com/kuan/rest/ResourceRouter.java
+++ restful-service/src/main/java/com/kuan/rest/ResourceRouter.java
@@ -115,32 +115,33 @@ class ResourceMethods {
                 .flatMap(methods -> Result.match(path, methods, r -> r.getRemaining() == null));
     }
 
-    static record Result<T extends ResourceRouter.UriHandler>
-            (Optional<UriTemplate.MatchResult> matched, T handler,
-             Function<UriTemplate.MatchResult, Boolean> matchFunction) implements Comparable<Result<T>> {
+}
 
-        // 真正想重用的方法
-        public static <T extends ResourceRouter.UriHandler> Optional<T>
-        match(String path, List<T> handlers, Function<UriTemplate.MatchResult, Boolean> matchFunction) {
-            return handlers.stream()
-                    .map(m -> new Result<>(m.getUriTemplate().match(path), m, matchFunction))
-                    .filter(Result::isMatched)
-                    .sorted().findFirst()
-                    .map(Result::handler);
-        }
+record Result<T extends ResourceRouter.UriHandler>
+        (Optional<UriTemplate.MatchResult> matched, T handler,
+         Function<UriTemplate.MatchResult, Boolean> matchFunction) implements Comparable<Result<T>> {
 
-        public static <T extends ResourceRouter.UriHandler> Optional<T> match(String path, List<T> handlers) {
-            return match(path, handlers, r -> true);
-        }
+    // 真正想重用的方法
+    public static <T extends ResourceRouter.UriHandler> Optional<T>
+    match(String path, List<T> handlers, Function<UriTemplate.MatchResult, Boolean> matchFunction) {
+        return handlers.stream()
+                .map(m -> new Result<>(m.getUriTemplate().match(path), m, matchFunction))
+                .filter(Result::isMatched)
+                .sorted().findFirst()
+                .map(Result::handler);
+    }
 
-        public boolean isMatched() {
-            return matched.map(matchFunction).orElse(false);
-        }
+    public static <T extends ResourceRouter.UriHandler> Optional<T> match(String path, List<T> handlers) {
+        return match(path, handlers, r -> true);
+    }
 
-        @Override
-        public int compareTo(Result<T> o) {
-            return matched.flatMap(x -> o.matched.map(x::compareTo)).orElse(0);
-        }
+    public boolean isMatched() {
+        return matched.map(matchFunction).orElse(false);
+    }
+
+    @Override
+    public int compareTo(Result<T> o) {
+        return matched.flatMap(x -> o.matched.map(x::compareTo)).orElse(0);
     }
 }
 
