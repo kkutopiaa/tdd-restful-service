10d56a39ae3262954cf0dc6ff0df39bb6d9518d0
qxkk <qinxuekuan@qianxin.com>
2022-10-13 19:57:11 +0800

    62th。简单搭建 should_call_locator_method_to_generate_sub_resource 测试的框架

--- restful-service/src/test/java/com/kuan/rest/SubResourceLocatorsTest.java
+++ restful-service/src/test/java/com/kuan/rest/SubResourceLocatorsTest.java
@@ -1,8 +1,12 @@
 package com.kuan.rest;
 
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.MediaType;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -30,6 +34,22 @@ public class SubResourceLocatorsTest {
     }
 
 
+    @Test
+    public void should_call_locator_method_to_generate_sub_resource() {
+        SubResourceLocators locators = new SubResourceLocators(Messages.class.getMethods());
+        ResourceRouter.SubResourceLocator subResourceLocator = locators.findSubResource("/hello").get();
+
+        // 还没有 getSubResource 方法，编译不会通过
+        ResourceRouter.Resource subResource = subResourceLocator.getSubResource();
+
+        UriTemplate.MatchResult result = Mockito.mock(UriTemplate.MatchResult.class);
+        Mockito.when(result.getRemaining()).thenReturn(null);
+        ResourceRouter.ResourceMethod method = subResource.match(result, "GET",
+                new String[]{MediaType.TEXT_PLAIN}, Mockito.mock(UriInfoBuilder.class)).get();
+
+        assertEquals("Message.content", method.toString());
+    }
+
 
     @Path("/messages")
     static class Messages {
@@ -42,6 +62,11 @@ public class SubResourceLocatorsTest {
     }
 
     static class Message {
+
+        @GET
+        public String content() {
+            return "content";
+        }
 
     }
 
