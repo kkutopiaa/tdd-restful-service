e5aca355faf3cac1b597c01cfa2527ef01e71977
qxkk <qinxuekuan@qianxin.com>
2022-11-03 19:38:57 +0800

    69th。添加 should_use_resource_method_generic_return_type 测试。实现逻辑之前已有

--- restful-service/src/test/java/com/kuan/rest/DefaultResourceMethodTest.java
+++ restful-service/src/test/java/com/kuan/rest/DefaultResourceMethodTest.java
@@ -4,8 +4,11 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.container.ResourceContext;
 import jakarta.ws.rs.core.GenericEntity;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
@@ -17,22 +20,54 @@ import static org.mockito.Mockito.when;
  */
 public class DefaultResourceMethodTest {
 
-    @Test
-    public void should_call_resource_method() throws NoSuchMethodException {
-        CallableResourceMethods resource = mock(CallableResourceMethods.class);
-        ResourceContext context = mock(ResourceContext.class);
-        UriInfoBuilder builder = mock(UriInfoBuilder.class);
+    private CallableResourceMethods resource;
+    private ResourceContext context;
+    private UriInfoBuilder builder;
+
+    @BeforeEach
+    public void before() {
+        resource = mock(CallableResourceMethods.class);
+        context = mock(ResourceContext.class);
+        builder = mock(UriInfoBuilder.class);
         when(builder.getLastMatchedResource()).thenReturn(resource);
+    }
+
+    @Test
+    public void should_call_resource_method() {
         when(resource.get()).thenReturn("resource called");
 
-        DefaultResourceMethod resourceMethod =
-                new DefaultResourceMethod(CallableResourceMethods.class.getMethod("get"));
+        DefaultResourceMethod resourceMethod = getResourceMethod("get");
+
         assertEquals(new GenericEntity("resource called", String.class), resourceMethod.call(context, builder));
+    }
+
+
+    @Test
+    public void should_use_resource_method_generic_return_type() throws NoSuchMethodException {
+        when(resource.getList()).thenReturn(List.of());
+
+        DefaultResourceMethod resourceMethod = getResourceMethod("getList");
+
+        assertEquals(
+                new GenericEntity<>(List.of(), CallableResourceMethods.class.getMethod("getList").getGenericReturnType()),
+                resourceMethod.call(context, builder));
+    }
+
+
+    private DefaultResourceMethod getResourceMethod(String methodName) {
+        try {
+            return new DefaultResourceMethod(CallableResourceMethods.class.getMethod(methodName));
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     interface CallableResourceMethods {
         @GET
         String get();
+
+        @GET
+        List<String> getList();
     }
 
 }
