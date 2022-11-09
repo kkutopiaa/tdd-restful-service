daf33bd7a62adbf679e851180b15f6006e4ee449
qxkk <qinxuekuan@qianxin.com>
2022-11-09 13:16:53 +0800

    75th。重构。目的：尝试使用继承，复用 SubResourceLocatorTest、DefaultResourceMethodTest 测试里的类似结构。 --> 将这 2 个类中的相同逻辑测试代码 pull up 到父类中。

--- restful-service/src/test/java/com/kuan/rest/DefaultResourceMethodTest.java
+++ restful-service/src/test/java/com/kuan/rest/DefaultResourceMethodTest.java
@@ -8,10 +8,7 @@ import jakarta.ws.rs.container.ResourceContext;
 import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.GenericEntity;
 import jakarta.ws.rs.core.UriInfo;
-import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestFactory;
-import org.junit.jupiter.api.function.Executable;
 
 import java.lang.reflect.Parameter;
 import java.lang.reflect.Proxy;
@@ -22,8 +19,6 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.mock;
 
 /**
  * @Author: qxkk
@@ -67,70 +62,8 @@ public class DefaultResourceMethodTest extends InjectableCallerTest{
                 resourceMethod.call(context, builder));
     }
 
-
-    record InjectableTypeTestCase(Class<?> type, String string, Object value) {
-
-    }
-
-    @TestFactory
-    public List<DynamicTest> inject_convertible_types() {
-        List<DynamicTest> tests = new ArrayList<>();
-
-        List<String> paramTypes = List.of("getPathParam", "getQueryParam");
-        List<InjectableTypeTestCase> typeCases = List.of(
-                new InjectableTypeTestCase(String.class, "string", "string"),
-                new InjectableTypeTestCase(double.class, "3.14", 3.14),
-                new InjectableTypeTestCase(float.class, "1.1", (float) 1.1),
-                new InjectableTypeTestCase(long.class, "2", (long) 2),
-                new InjectableTypeTestCase(int.class, "1", 1),
-                new InjectableTypeTestCase(short.class, "128", (short) 128),
-                new InjectableTypeTestCase(byte.class, "127", (byte) 127),
-                new InjectableTypeTestCase(boolean.class, "TRUE", true),
-                new InjectableTypeTestCase(BigDecimal.class, "12345", new BigDecimal("12345")),
-                new InjectableTypeTestCase(Converter.class, "Factory", Converter.Factory)
-        );
-
-        for (String type : paramTypes) {
-            for (InjectableTypeTestCase testCase : typeCases) {
-                String displayName = "should inject " + testCase.type().getSimpleName() + " to " + type;
-                Executable executable =
-                        () -> verifyResourceMethodCalled(type, testCase.type(), testCase.string(), testCase.value());
-                tests.add(DynamicTest.dynamicTest(displayName, executable));
-            }
-        }
-
-        return tests;
-    }
-
-    @TestFactory
-    public List<DynamicTest> inject_context_object() {
-        List<DynamicTest> tests = new ArrayList<>();
-        List<InjectableTypeTestCase> typeCases = List.of(
-                new InjectableTypeTestCase(SameServiceInContext.class, "N/A", service),
-                new InjectableTypeTestCase(ResourceContext.class, "N/A", context),
-                new InjectableTypeTestCase(UriInfo.class, "N/A", uriInfo)
-        );
-
-        for (InjectableTypeTestCase typeCase : typeCases) {
-            String displayName = "should inject " + typeCase.type().getSimpleName() + " to getContext";
-            Executable executable =
-                    () -> verifyResourceMethodCalled("getContext", typeCase.type(), typeCase.string(), typeCase.value());
-            tests.add(DynamicTest.dynamicTest(displayName, executable));
-        }
-
-        return tests;
-    }
-
-    private void verifyResourceMethodCalled(String method, Class<?> type, String paramString, Object paramValue) {
-        parameters.put("param", List.of(paramString));
-
-        callInjectable(method, type);
-
-        assertEquals(getMethodName(method, List.of(type)), lastCall.name());
-        assertEquals(List.of(paramValue), lastCall.arguments());
-    }
-
-    private void callInjectable(String method, Class<?> type) {
+    @Override
+    protected void callInjectable(String method, Class<?> type) {
         DefaultResourceMethod resourceMethod = getResourceMethod(method, type);
         resourceMethod.call(context, builder);
     }
--- restful-service/src/test/java/com/kuan/rest/InjectableCallerTest.java
+++ restful-service/src/test/java/com/kuan/rest/InjectableCallerTest.java
@@ -4,10 +4,16 @@ import jakarta.ws.rs.container.ResourceContext;
 import jakarta.ws.rs.core.MultivaluedHashMap;
 import jakarta.ws.rs.core.UriInfo;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.TestFactory;
+import org.junit.jupiter.api.function.Executable;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -53,7 +59,71 @@ public abstract class InjectableCallerTest {
                 + ")";
     }
 
+    @TestFactory
+    public List<DynamicTest> inject_convertible_types() {
+        List<DynamicTest> tests = new ArrayList<>();
+
+        List<String> paramTypes = List.of("getPathParam", "getQueryParam");
+        List<InjectableTypeTestCase> typeCases = List.of(
+                new InjectableTypeTestCase(String.class, "string", "string"),
+                new InjectableTypeTestCase(double.class, "3.14", 3.14),
+                new InjectableTypeTestCase(float.class, "1.1", (float) 1.1),
+                new InjectableTypeTestCase(long.class, "2", (long) 2),
+                new InjectableTypeTestCase(int.class, "1", 1),
+                new InjectableTypeTestCase(short.class, "128", (short) 128),
+                new InjectableTypeTestCase(byte.class, "127", (byte) 127),
+                new InjectableTypeTestCase(boolean.class, "TRUE", true),
+                new InjectableTypeTestCase(BigDecimal.class, "12345", new BigDecimal("12345")),
+                new InjectableTypeTestCase(Converter.class, "Factory", Converter.Factory)
+        );
+
+        for (String type : paramTypes) {
+            for (InjectableTypeTestCase testCase : typeCases) {
+                String displayName = "should inject " + testCase.type().getSimpleName() + " to " + type;
+                Executable executable =
+                        () -> verifyResourceMethodCalled(type, testCase.type(), testCase.string(), testCase.value());
+                tests.add(DynamicTest.dynamicTest(displayName, executable));
+            }
+        }
+
+        return tests;
+    }
+
+    @TestFactory
+    public List<DynamicTest> inject_context_object() {
+        List<DynamicTest> tests = new ArrayList<>();
+        List<InjectableTypeTestCase> typeCases = List.of(
+                new InjectableTypeTestCase(SameServiceInContext.class, "N/A", service),
+                new InjectableTypeTestCase(ResourceContext.class, "N/A", context),
+                new InjectableTypeTestCase(UriInfo.class, "N/A", uriInfo)
+        );
+
+        for (InjectableTypeTestCase typeCase : typeCases) {
+            String displayName = "should inject " + typeCase.type().getSimpleName() + " to getContext";
+            Executable executable =
+                    () -> verifyResourceMethodCalled("getContext", typeCase.type(), typeCase.string(), typeCase.value());
+            tests.add(DynamicTest.dynamicTest(displayName, executable));
+        }
+
+        return tests;
+    }
+
+    private void verifyResourceMethodCalled(String method, Class<?> type, String paramString, Object paramValue) {
+        parameters.put("param", List.of(paramString));
+
+        callInjectable(method, type);
+
+        assertEquals(getMethodName(method, List.of(type)), lastCall.name());
+        assertEquals(List.of(paramValue), lastCall.arguments());
+    }
+
+    protected abstract void callInjectable(String method, Class<?> type);
+
     record LastCall(String name, List<Object> arguments) {
+
+    }
+
+    record InjectableTypeTestCase(Class<?> type, String string, Object value) {
 
     }
 }
