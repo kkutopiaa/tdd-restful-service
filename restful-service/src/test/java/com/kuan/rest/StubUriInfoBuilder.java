4d4f4fb8a0426927899a6e2985f2714411a852d7
qxkk <qinxuekuan@qianxin.com>
2022-11-10 20:00:52 +0800

    77/78th。不用 mock 的 uriInfo，实现 should_add_last_match_path_parameters_uri_info_builder 测试。

--- restful-service/src/test/java/com/kuan/rest/StubUriInfoBuilder.java
+++ restful-service/src/test/java/com/kuan/rest/StubUriInfoBuilder.java
@@ -1,9 +1,8 @@
 package com.kuan.rest;
 
-import jakarta.ws.rs.core.MultivaluedHashMap;
-import jakarta.ws.rs.core.MultivaluedMap;
-import jakarta.ws.rs.core.UriInfo;
+import jakarta.ws.rs.core.*;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -39,11 +38,6 @@ class StubUriInfoBuilder implements UriInfoBuilder {
     }
 
     @Override
-    public UriInfo createUriInfo() {
-        return uriInfo;
-    }
-
-    @Override
     public void addMatchedPathParameters(Map<String, String> pathParameters) {
         for (String key : pathParameters.keySet()) {
             this.paramters.add(key, pathParameters.get(key));
@@ -52,6 +46,106 @@ class StubUriInfoBuilder implements UriInfoBuilder {
 
     public MultivaluedMap<String, String> getPathParameters() {
         return paramters;
+    }
+
+    @Override
+    public UriInfo createUriInfo() {
+        return new UriInfo() {
+            @Override
+            public String getPath() {
+                return null;
+            }
+
+            @Override
+            public String getPath(boolean decode) {
+                return null;
+            }
+
+            @Override
+            public List<PathSegment> getPathSegments() {
+                return null;
+            }
+
+            @Override
+            public List<PathSegment> getPathSegments(boolean decode) {
+                return null;
+            }
+
+            @Override
+            public URI getRequestUri() {
+                return null;
+            }
+
+            @Override
+            public UriBuilder getRequestUriBuilder() {
+                return null;
+            }
+
+            @Override
+            public URI getAbsolutePath() {
+                return null;
+            }
+
+            @Override
+            public UriBuilder getAbsolutePathBuilder() {
+                return null;
+            }
+
+            @Override
+            public URI getBaseUri() {
+                return null;
+            }
+
+            @Override
+            public UriBuilder getBaseUriBuilder() {
+                return null;
+            }
+
+            @Override
+            public MultivaluedMap<String, String> getPathParameters() {
+                return paramters;
+            }
+
+            @Override
+            public MultivaluedMap<String, String> getPathParameters(boolean decode) {
+                return null;
+            }
+
+            @Override
+            public MultivaluedMap<String, String> getQueryParameters() {
+                return null;
+            }
+
+            @Override
+            public MultivaluedMap<String, String> getQueryParameters(boolean decode) {
+                return null;
+            }
+
+            @Override
+            public List<String> getMatchedURIs() {
+                return null;
+            }
+
+            @Override
+            public List<String> getMatchedURIs(boolean decode) {
+                return null;
+            }
+
+            @Override
+            public List<Object> getMatchedResources() {
+                return null;
+            }
+
+            @Override
+            public URI resolve(URI uri) {
+                return null;
+            }
+
+            @Override
+            public URI relativize(URI uri) {
+                return null;
+            }
+        };
     }
 
 }
